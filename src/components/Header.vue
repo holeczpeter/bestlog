38acb2eb5911d491e381dab2109324c510dea482
holeczpeter <holecz.peter85@gmail.com>
2024-02-04 23:10:48 +0100

    update header routes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -45,12 +45,12 @@
                               <span v-if="item.hasChildren" 
                                  class="submenu-button">
                               </span>
-                              <a :href="route.meta.basePath  + '/'" v-scroll-to="item.routerlink" v-if=isHome>{{ item.title }}</a>
-                              <a :href="route.meta.basePath  + '/' + item.routerlink" v-if=!isHome>{{ item.title }}</a>
+                              <a :href="`${$route.meta.basePath || ''}/`" v-scroll-to="item.routerlink" v-if=isHome>{{ item.title }}</a>
+                              <a :href="`${$route.meta.basePath || ''}/` + item.routerlink" v-if=!isHome>{{ item.title }}</a>
                               <ul v-if="item.hasChildren" :class="{ 'open': item.open }" :style="{display: item.open ? 'block': 'none'}">
                                  <li v-for="(child, index) in item.children" 
                                     :key="index" class="sub-menu">
-                                    <a :href="'services/' + child.id">{{child.title}}</a> 
+                                    <a :href="`${$route.meta.basePath || ''}/` + 'services/' + child.id">{{child.title}}</a> 
                                  </li>
                               </ul>
                            </li>
