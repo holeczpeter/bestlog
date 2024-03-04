f56d371253ac021313a14da7569922810641ebe5
holeczpeter <holecz.peter85@gmail.com>
2024-03-04 18:32:33 +0100

    partners html fix

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,20 +1,13 @@
 <template>
- <section class="section-slider">
+    <section class="section-slider">
          <div class="text-center mb-5 w-95 w-md-90 w-lg-70 w-xl-60 mx-auto wow fadeIn" data-wow-delay="200ms">
             <span class="text-uppercase letter-spacing-2 font-weight-700 small mb-3 d-block">Partnereink</span>
-           
          </div>
         <div class="slider">
             <div class="slider-items">
-                <img src="../assets/img/logos/wimmer.avif"
-                    alt="">
-                <img src="../assets/img/logos/fafem.png"
-                    alt="">
-                
-
-
+                <img src="../assets/img/logos/wimmer.avif" alt="">
+                <img src="../assets/img/logos/fafem.png" alt="">
             </div>
         </div>
-
     </section>
 </template>
